d7fc410025f7d4853438ce6958437369b7edf509
Matthew Waanders <matthew@athom.nl>
2018-11-14 10:45:29 +0100

    Fixes flows for push button and co sensor

--- drivers/FGGC-001/driver.js
+++ drivers/FGGC-001/driver.js
@@ -8,19 +8,19 @@ class FibaroSwipeDeviceDriver extends Homey.Driver {
 
         this.directionTrigger = new Homey.FlowCardTriggerDevice('fggc-001_swipe_direction').register()
             .registerRunListener((args, state) => {
-                args.device.directionRunListener(args, state);
+                return args.device.directionRunListener(args, state);
             });
 
         this.roundTrigger = new Homey.FlowCardTriggerDevice('fggc-001_swipe_round')
             .register()
             .registerRunListener((args, state) => {
-                args.device.roundRunListener(args, state);
+                return args.device.roundRunListener(args, state);
             });
 
         this.sequenceTrigger = new Homey.FlowCardTriggerDevice('fggc-001_swipe_sequence')
             .register()
             .registerRunListener((args, state) => {
-                args.device.sequenceRunListener(args, state);
+                return args.device.sequenceRunListener(args, state);
             });
     }
 }
