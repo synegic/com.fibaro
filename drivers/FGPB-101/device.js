91c8206d356ca2203f855c16df28d7758eafecf8
Matthew Waanders <matthew@athom.nl>
2019-01-03 14:53:24 +0100

    Fixed push button not working

--- drivers/FGPB-101/device.js
+++ drivers/FGPB-101/device.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const Homey = require('homey');
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class Button extends ZwaveDevice {
@@ -9,36 +8,30 @@ class Button extends ZwaveDevice {
 		this.registerCapability('measure_battery', 'BATTERY');
 		this._onButtonTrigger = this.getDriver().onButtonTrigger;
 
-		this.node.CommandClass.COMMAND_CLASS_CENTRAL_SCENE.on('report', (command, report) => {
-			let debouncer = 0;
+		this.registerReportListener('CENTRAL_SCENE', 'CENTRAL_SCENE_NOTIFICATION', (report) => {
+            let debouncer = 0;
 
-			if (command.name === 'CENTRAL_SCENE_NOTIFICATION') {
-				if (report &&
-					report.Properties1.hasOwnProperty('Key Attributes')) {
-					const buttonValue = { scene: report.Properties1['Key Attributes'] };
-					if (buttonValue.scene === 'Key Released') {
-						if (debouncer === 0) {
-							this._onButtonTrigger.trigger(this, null, buttonValue);
-
-							debouncer++;
-							setTimeout(() => debouncer = 0, 2000);
-						}
-					} else {
+            if (report &&
+				report.Properties1.hasOwnProperty('Key Attributes')) {
+				const buttonValue = { scene: report.Properties1['Key Attributes'] };
+				if (buttonValue.scene === 'Key Released') {
+					if (debouncer === 0) {
 						this._onButtonTrigger.trigger(this, null, buttonValue);
+
+						debouncer++;
+						setTimeout(() => debouncer = 0, 2000);
 					}
+				} else {
+					this._onButtonTrigger.trigger(this, null, buttonValue);
 				}
 			}
 		});
 	}
 
 	buttonRunListener(args, state) {
-        if (state && args &&
+        return (state && args &&
             state.hasOwnProperty('scene') &&
-            args.hasOwnProperty('scene')) {
-            return Promise.resolve();
-        }
-
-        return Promise.reject();
+            args.hasOwnProperty('scene'))
 	}
 
 }
