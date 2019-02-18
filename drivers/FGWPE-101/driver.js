f44f02e5862e2723d279b1730abb8cec7d4f6148
Matthew Waanders <matthew@athom.nl>
2019-02-18 21:26:23 +0100

    Fixes small issues

--- drivers/FGWPE-101/driver.js
+++ drivers/FGWPE-101/driver.js
@@ -6,10 +6,10 @@ class FibaroWallPlugDriver extends Homey.Driver {
     onInit() {
         super.onInit();
 
-        this.ledOnAction = new Homey.FlowCardTriggerDevice('FGWPE_led_on').register().registerRunListener((args, state) => {
+        this.ledOnAction = new Homey.FlowCardAction('FGWPE_led_on').register().registerRunListener((args, state) => {
             return args.device.ledOnRunListener(args, state);
         });
-        this.ledOffAction = new Homey.FlowCardTriggerDevice('FGWPE_led_off').register().registerRunListener((args, state) => {
+        this.ledOffAction = new Homey.FlowCardAction('FGWPE_led_off').register().registerRunListener((args, state) => {
             return args.device.ledOffRunListener(args, state);
         });
     }
--- drivers/FGWPx-102-PLUS/driver.js
+++ drivers/FGWPx-102-PLUS/driver.js
@@ -6,10 +6,10 @@ class FibaroWallPlugPlusDriver extends Homey.Driver {
     onInit() {
         super.onInit();
 
-        this.ledOnAction = new Homey.FlowCardTriggerDevice('FGWPx-102-PLUS_led_on').register().registerRunListener(async (args, state) => {
+        this.ledOnAction = new Homey.FlowCardAction('FGWPx-102-PLUS_led_on').register().registerRunListener(async (args, state) => {
             return args.device.ledOnRunListener(args, state);
         });
-        this.ledOffAction = new Homey.FlowCardTriggerDevice('FGWPx-102-PLUS_led_off').register().registerRunListener(async (args, state) => {
+        this.ledOffAction = new Homey.FlowCardAction('FGWPx-102-PLUS_led_off').register().registerRunListener(async (args, state) => {
             return args.device.ledOffRunListener(args, state);
         });
     }
