8271508013af041e8ecfd7b6266058492bc53398
Matthew Waanders <matthew@athom.nl>
2019-03-18 09:41:49 +0100

    Fix to always_on setting for non Z-Wave plus wall plug

--- drivers/FGWPE-101/device.js
+++ drivers/FGWPE-101/device.js
@@ -8,6 +8,11 @@ class FibaroWallPlug extends ZwaveDevice {
 		this.registerCapability('onoff', 'SWITCH_BINARY');
 		this.registerCapability('measure_power', 'SENSOR_MULTILEVEL');
 		this.registerCapability('meter_power', 'METER');
+
+		this.registerSetting('always_on', value => {
+		    // Flip 0 = 1, 1 = 0, because 0 is active and 1 is inactive
+		    return new Buffer([value === true ? 0 : 1])
+        });
 	}
 
     async ledOnRunListener(args, state) {
