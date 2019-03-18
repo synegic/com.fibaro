de0bc3239d1074f728547758dca50d82ba63c795
Matthew Waanders <matthew@athom.nl>
2019-03-18 14:30:22 +0100

    Added associations for th FGSD-002. getOnOnline for all alarms

--- drivers/FGSD-002/device.js
+++ drivers/FGSD-002/device.js
@@ -5,8 +5,12 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class FibaroSmokeDetectorDevice extends ZwaveDevice {
 
 	onMeshInit() {
-		this.registerCapability('alarm_smoke', 'NOTIFICATION');
-		this.registerCapability('alarm_heat', 'NOTIFICATION');
+		this.registerCapability('alarm_smoke', 'SENSOR_ALARM', {
+			getOnOnline: true
+		});
+		this.registerCapability('alarm_heat', 'SENSOR_ALARM', {
+			getOnOnline: true
+		});
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
 		this.registerCapability('measure_battery', 'BATTERY');
 	}
--- drivers/FGSS-001/device.js
+++ drivers/FGSS-001/device.js
@@ -6,11 +6,15 @@ class FibaroSmokeSensor extends ZwaveDevice {
 
 	onMeshInit() {
 		this.registerCapability('measure_battery', 'BATTERY');
-		this.registerCapability('alarm_smoke', 'SENSOR_ALARM');
+		this.registerCapability('alarm_smoke', 'SENSOR_ALARM', {
+            getOnOnline: true
+        });
 		this.registerCapability('alarm_heat', 'SENSOR_ALARM', {
 			getOnOnline: true
 		});
-		this.registerCapability('alarm_tamper', 'SENSOR_ALARM');
+		this.registerCapability('alarm_tamper', 'SENSOR_ALARM', {
+            getOnOnline: true
+        });
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
 	}
 
