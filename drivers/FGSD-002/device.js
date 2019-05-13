e6af76028150796a4a151fbb48ea8280af14b809
Matthew Waanders <matthew@athom.nl>
2019-05-13 14:24:44 +0200

    Fixed temperature alarm for FGSD-002

--- drivers/FGSD-002/device.js
+++ drivers/FGSD-002/device.js
@@ -6,14 +6,14 @@ class FibaroSmokeDetectorDevice extends ZwaveDevice {
 
 	onMeshInit() {
 		this.registerCapability('alarm_smoke', 'SENSOR_ALARM');
-        this.registerReportListener('SENSOR_ALARM', 'NOTIFICATION', report => {
+        this.registerReportListener('NOTIFICATION', 'NOTIFICATION_REPORT', report => {
             if (report && report['Notification Type'] === 'Smoke') {
                 this.setCapabilityValue('alarm_smoke', report['Event'] === 1 || report['Event'] === 2 || report['Event'] === 3);
             }
 		});
 
         this.registerCapability('alarm_heat', 'SENSOR_ALARM');
-        this.registerReportListener('SENSOR_ALARM', 'NOTIFICATION', report => {
+        this.registerReportListener('NOTIFICATION', 'NOTIFICATION_REPORT', report => {
             if (report && report['Notification Type'] === 'Heat') {
                 this.setCapabilityValue('alarm_heat', report['Event'] === 1 || report['Event'] === 2 || report['Event'] === 3 || report['Event'] === 4 || report['Event'] === 7);
             }
