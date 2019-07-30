d80e53e191941a58aba31339bd9cbc2524fd3bc2
Kyle Taylor Parkins <kyle@athom.nl>
2019-07-30 14:01:19 +0200

    Added parameters for Walli Roller Shutter
    Hide roller shutter since calibration doesn't work correctly.

--- drivers/FGWREU-111/device.js
+++ drivers/FGWREU-111/device.js
@@ -5,6 +5,16 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class FibaroRollerShutterDevice extends ZwaveDevice {
 	onMeshInit() {
 		this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+
+		this.registerSetting('start_calibration', (newValue) => {
+			if (newValue) {
+				setTimeout(() => {
+					this.setSettings({ start_calibration: false });
+				}, 5000);
+			}
+
+			return new Buffer([newValue ? 2 : 0]);
+		});
 	}
 }
 
