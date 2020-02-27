ac482c4ad680ea29c40c1cf1875c1456164be33c
Kyle Athom <kyle@athom.nl>
2020-02-27 08:59:26 +0100

    FGS-213 Fixes

--- drivers/FGS-213/driver.js
+++ drivers/FGS-213/driver.js
@@ -6,14 +6,17 @@ class FibaroSingleSwitchTwoDevice extends Homey.Driver {
     onInit() {
         super.onInit();
 
-        this.S1Trigger = new Homey.FlowCardTriggerDevice('FGS-213_S1').register().registerRunListener((args, state) => {
-            return args.device.switchTriggerRunListener(args, state);
+        this.S1Trigger = new Homey.FlowCardTriggerDevice('FGS-213_S1').register()
+            .registerRunListener((args, state) => {
+                return args.device.switchTriggerRunListener(args, state);
         });
-        this.S2Trigger = new Homey.FlowCardTriggerDevice('FGS-213_S2').register().registerRunListener((args, state) => {
-            return args.device.switchTriggerRunListener(args, state);
+        this.S2Trigger = new Homey.FlowCardTriggerDevice('FGS-213_S2').register()
+            .registerRunListener((args, state) => {
+                return args.device.switchTriggerRunListener(args, state);
         });
-        this.resetMeter = new Homey.FlowCardAction('FGS-213_reset_meter').register().registerRunListener((args, state) => {
-            return args.device.resetMeterRunListener(args, state);
+        this.resetMeter = new Homey.FlowCardAction('FGS-213_reset_meter').register()
+            .registerRunListener((args, state) => {
+                return args.device.resetMeterRunListener(args, state);
         });
     }
 }
