b5cfc925fb39a47471fa032a0c22e6dbe2558826
KyleAthom <kyle@athom.com>
2020-03-03 14:28:57 +0100

    Added RGBW2 onoff/dim

--- drivers/FGRGBWM-442/device.js
+++ drivers/FGRGBWM-442/device.js
@@ -1,14 +1,12 @@
 'use strict'
 
 const ZwaveLightDevice = require('homey-meshdriver').ZwaveLightDevice;
-const ZwaveUtils = require('homey-meshdriver').Util;
-
-const tinyGradient = require('tinygradient');
 
 class FibaroRGBW2Device extends ZwaveLightDevice {
-    onMeshInit() {
-        super.onMeshInit();
-        this.enableDebug();
+    async onMeshInit() {
+        await super.onMeshInit();
+
+        // this.printNode();
     }
 }
 
