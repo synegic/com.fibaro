0d1eab6042ada0d87c55c52ea2c07f0f79b8628f
KyleAthom <kyle@athom.com>
2020-03-18 14:31:09 +0100

    Update Walli Flows

--- drivers/FGWOF-011/driver.js
+++ drivers/FGWOF-011/driver.js
@@ -6,10 +6,10 @@ class FibaroWalliWallOutletDriver extends Homey.Driver {
     onInit() {
         super.onInit();
 
-        this.ledOnAction = new Homey.FlowCardAction('FGWOF_led_on').register().registerRunListener((args, state) => {
+        this.ledOnAction = new Homey.FlowCardAction('walli_led_on').register().registerRunListener((args, state) => {
             return args.device.ledOnRunListener(args, state);
         });
-        this.ledOffAction = new Homey.FlowCardAction('FGWOF_led_off').register().registerRunListener((args, state) => {
+        this.ledOffAction = new Homey.FlowCardAction('walli_led_off').register().registerRunListener((args, state) => {
             return args.device.ledOffRunListener(args, state);
         });
     }
